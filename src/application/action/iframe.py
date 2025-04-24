d037e71a3dd2dc3985bc9d28b45f0691b8ab3bc0
SottoMonte <mario423434@gmail.com>
2025-04-24 18:00:21 +0200

    fix iframe

--- src/application/action/iframe.py
+++ src/application/action/iframe.py
@@ -7,6 +7,10 @@ modules = {'flow': 'framework.service.flow'}
 async def iframe(messenger, presenter, storekeeper, **constants):
     target = constants.get('id', 'myIframe')  # ID dell'iframe
     iframe_url = constants.get('src', '')     # URL da caricare
+    if 'target' in constants:
+        target = constants['target']
+        v = js.document.getElementById(target)
+        iframe_url = v.value
 
     print(f"Updating iframe '{target}' to load: {iframe_url}")
     
