12b2b5452eb0e925a4f6c6acdbc9ddf0798e10c4
SottoMonte <mario423434@gmail.com>
2025-05-08 14:17:46 +0200

    fix chat

--- src/application/action/chat.py
+++ src/application/action/chat.py
@@ -1,29 +1,28 @@
 modules = {'flow': 'framework.service.flow'}
 
+import os
+
 @flow.asynchronous(managers=('messenger', 'presenter', 'storekeeper'))
-async def chat(messenger, presenter,storekeeper, **constants):
-    print(constants)
+async def chat(messenger, presenter, storekeeper, **constants):
     component = await presenter.component(name='chat-ai')
     file = constants.get('file', '')
+    location = constants.get('location', '')
+    message = constants.get('message', '')
 
-    payload = {'location':constants.get('location',''),'path':file}
     doc = ''
-    files = []
-    if 'src/application/view' in file:
-        a = await storekeeper.gather(repository='file', payload=payload)
-        files |= a.get('result',[])
+    paths = [file]
+    directory = os.path.dirname(file.replace('src/application/', ''))
+    path = 'src/application'
+    for x in directory.split('/'):
+        path = f"{path}/{x}"
+        paths.append(path+f'/{x}.md')
+    
+    print('paths:', paths)
 
-    a = await storekeeper.gather(repository='file', payload=payload)
-    files |= a.get('result',[])
+    for path in paths:
+        payload = {'location': location, 'path': path}
+        result = await storekeeper.gather(repository='file', payload=payload)
+        doc += ''.join(f['content'] for f in result.get('result', []))
 
-    for file in files:
-        doc += file['content']
-    #print(a)
-    constants.pop('file', None)
-    constants.pop('location', None)
-    #print(constants,'data')
-    #print('component',component)
-    component.setdefault('messenger',[]).append(constants)
-    question = constants.get('message', '')
-    ok = await messenger.post(domain='chat', message=f"Usa il seguente documento come base per rispondere alle domande dell'utente:\n\n{doc}"+question)
-    #print(ok,question,'CHAT')
+    component.setdefault('messenger', []).append(constants)
+    await messenger.post(domain='chat', message=f"Usa il seguente documento come base per rispondere alle domande dell'utente:\n\n{doc} Utente msg:{message}")
